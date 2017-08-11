fcd89edb4aad3cb823916365651900ea4ce4abab
KavithaRamm <kavithabtechit@gmail.com>
2017-08-11 12:18:29 -0400

    Refactored version, not updating in the DB

--- jsontosql/src/main/java/com/riis/dao/JDBCPreparedStatementSelectExample.java
+++ jsontosql/src/main/java/com/riis/dao/JDBCPreparedStatementSelectExample.java
@@ -8,96 +8,78 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class JDBCPreparedStatementSelectExample {
-
 	private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String DB_CONNECTION = "jdbc:mysql://localhost/eta";
 	private static final String DB_USER = "root";
 	private static final String DB_PASSWORD = "abcd4561";
+	private Connection dbConnection = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	private String direction = "northbound";
 
-	public void insertIntoSmartBusSchedulesTable(String stopName, List<String> times) throws SQLException {
-		Connection dbConnection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-		String direction = "northbound";
-
+	public void insertIntoDBTable(String stopName, List<String> times) throws SQLException {
 		for (int i = 0; i < times.size(); i++) {
-
-			String selectSQL = "SELECT distinct stopId FROM Smart_Bus_Schedules where stp_name =" + "'" + stopName + "'" + " and direction =" + "'"
-					+ direction + "'";
-			try {
-				dbConnection = getDBConnection();
-				preparedStatement = dbConnection.prepareStatement(selectSQL);
-				resultSet = preparedStatement.executeQuery();
-
-			} catch (SQLException e) {
-
-				System.out.println(e.getMessage());
-
-			}
+			resultSet = getStopID(stopName);
 			while (resultSet.next()) {
-				String stopID = resultSet.getString("stopId");
-				System.out.println("stopID : " + stopID);
-				String insertSQL = "insert into Smart_Bus_Schedules values('SmartBus', '125', 'NORTHBOUND', 'SUNDAY',?,?, ?, NULL)";
-				try {
-					dbConnection = getDBConnection();
-					preparedStatement = dbConnection.prepareStatement(insertSQL);
-					preparedStatement.setString(1, stopID);
-					preparedStatement.setString(2, stopName);
-					preparedStatement.setString(3, times.get(i));
-					// execute select SQL statement
-					boolean entryResult = preparedStatement.execute();
-
-				} catch (SQLException e) {
-
-					System.out.println(e.getMessage());
-
-				} finally {
-
-					if (preparedStatement != null) {
-						preparedStatement.close();
-					}
-
-					if (dbConnection != null) {
-						dbConnection.close();
-					}
-
-				}
-
+				insertIntoSmartBusSchedulesTable(stopName, times, i);
 			}
 		}
-
 	}
 
-	private static Connection getDBConnection() {
-
-		Connection dbConnection = null;
-
+	/* Inserting into Smart_Bus_Schedules Table */
+	private void insertIntoSmartBusSchedulesTable(String stopName, List<String> times, int i) throws SQLException {
+		String stopID = resultSet.getString("stopId");
+		System.out.println("stopID : " + stopID);
+		String insertSQL = "insert into Smart_Bus_Schedules values('SmartBus', '125', 'NORTHBOUND', 'SUNDAY',?,?, ?, NULL)";
 		try {
-
-			Class.forName(DB_DRIVER);
-
-		} catch (ClassNotFoundException e) {
-
+			dbConnection = getDBConnection();
+			preparedStatement = dbConnection.prepareStatement(insertSQL);
+			preparedStatement.setString(1, stopID);
+			preparedStatement.setString(2, stopName);
+			preparedStatement.setString(3, times.get(i));
+			preparedStatement.execute();
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
-
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
 		}
+	}
 
+	/* Getting the stopID from DB */
+	private ResultSet getStopID(String stopName) {
+		String selectSQL = "SELECT distinct stopId FROM Smart_Bus_Schedules where stp_name =" + "'" + stopName + "'" + " and direction =" + "'"
+				+ direction + "'";
 		try {
-
-			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
-			return dbConnection;
+			dbConnection = getDBConnection();
+			preparedStatement = dbConnection.prepareStatement(selectSQL);
+			resultSet = preparedStatement.executeQuery();
 
 		} catch (SQLException e) {
-
 			System.out.println(e.getMessage());
-
 		}
+		return resultSet;
+	}
 
+	/* Creating DB connection */
+	private static Connection getDBConnection() {
+		Connection dbConnection = null;
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+			return dbConnection;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return dbConnection;
-
 	}
 
 }
-
-// SELECT distinct stopId FROM Smart_Bus_Schedules where stp_name ="+stopName+"
-// and direction ="+"northbound
--- jsontosql/src/main/java/com/riis/jsontosql/JsonToSql.java
+++ jsontosql/src/main/java/com/riis/jsontosql/JsonToSql.java
@@ -23,37 +23,21 @@ public class JsonToSql {
 
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void convertJsonToSql() {
 		JSONParser parser = new JSONParser();
 		Set<String> routeNumber = new HashSet<>();
 		JDBCPreparedStatementSelectExample jdbc = new JDBCPreparedStatementSelectExample();
 
 		try {
-			Object obj = parser.parse(new FileReader("/home/kavi/dumps/smartbus/route125SundayNorthbound"));
+			Object obj = parser.parse(new FileReader("/home/kavi/dumps/smartbus/route125SaturdaySouthbound"));
 			JSONArray jsonArray = (JSONArray) obj;
 			for (int i = 0; i < jsonArray.size(); i++) {
 				JSONObject jsonObj = (JSONObject) jsonArray.get(i);
 				String stopName = (String) jsonObj.get("Name");
 				System.out.println("Stop name " + stopName);
 				JSONArray timesJSONArray = (JSONArray) jsonObj.get("Times");
-				Iterator itr2 = timesJSONArray.iterator();
-				Iterator<Map.Entry> itr1 = null;
-				List<String> times = new ArrayList<>();
-				while (itr2.hasNext()) {
-					itr1 = ((Map) itr2.next()).entrySet().iterator();
-					while (itr1.hasNext()) {
-						Map.Entry pair = itr1.next();
-						if (pair.getKey().equals("RouteNumber") && routeNumber.size() == 0) {
-							routeNumber.add((String) pair.getValue());
-						}
-						if (pair.getKey().equals("Time")) {
-							times.add((String) pair.getValue());
-						}
-					}
-
-				}
-				jdbc.insertIntoSmartBusSchedulesTable(stopName, times);
+				List<String> times = getTimeFromJSON(routeNumber, timesJSONArray);
+				jdbc.insertIntoDBTable(stopName, times);
 				System.out.println(times);
 			}
 
@@ -61,5 +45,26 @@ public class JsonToSql {
 			e.printStackTrace();
 		}
 		System.out.println(routeNumber);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List<String> getTimeFromJSON(Set<String> routeNumber, JSONArray timesJSONArray) {
+		Iterator<Map.Entry> itr1;
+		Iterator itr2 = timesJSONArray.iterator();
+		List<String> times = new ArrayList<>();
+		while (itr2.hasNext()) {
+			itr1 = ((Map) itr2.next()).entrySet().iterator();
+			while (itr1.hasNext()) {
+				Map.Entry pair = itr1.next();
+				if (pair.getKey().equals("RouteNumber") && routeNumber.size() == 0) {
+					routeNumber.add((String) pair.getValue());
+				}
+				if (pair.getKey().equals("Time")) {
+					times.add((String) pair.getValue());
+				}
+			}
+
+		}
+		return times;
 	}
 }
